45831fdef630998c61a9718ade485defbcdc4928
sugutlaban4-lgtm <sugutlaban4@gmail.com>
2026-08-10 10:25:36 +0300

    Fix Firebase authentication

--- auth.js
+++ auth.js
@@ -1,56 +1,41 @@
 import { auth } from "./firebase.js";
 
-import { 
+import {
   createUserWithEmailAndPassword,
   updateProfile,
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-
-window.createAccount = function() {
-
+window.createAccount = function () {
   const name = document.getElementById("fullName").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-
   createUserWithEmailAndPassword(auth, email, password)
-
-  .then((userCredential) => {
-
-    updateProfile(userCredential.user, {
-      displayName: name
+    .then((userCredential) => {
+      return updateProfile(userCredential.user, {
+        displayName: name
+      });
+    })
+    .then(() => {
+      alert("Account created successfully!");
+      window.location = "login.html";
+    })
+    .catch((error) => {
+      alert(error.message);
     });
-
-    alert("Account created successfully!");
-    window.location = "login.html";
-
-  })
-
-  .catch((error) => {
-    alert(error.message);
-  });
-
 };
 
-
-window.loginUser = function() {
-
+window.loginUser = function () {
   const email = document.getElementById("loginEmail").value;
   const password = document.getElementById("loginPassword").value;
 
-
   signInWithEmailAndPassword(auth, email, password)
-
-  .then(() => {
-
-    alert("Login successful!");
-    window.location = "https://learnsmarthub.my.canva.site/dahroljpksa";
-
-  })
-
-  .catch((error) => {
-    alert(error.message);
-  });
-
+    .then(() => {
+      alert("Login successful!");
+      window.location = "dashboard.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 };
--- firebase.js
+++ firebase.js
@@ -4,11 +4,11 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 const firebaseConfig = {
   apiKey: "AIzaSyCTRqYqGMzqq6zvCiDijNSYdRwhqxVlXPo",
   authDomain: "learn-smart-hub-laban.firebaseapp.com",
-  projectId:"learn-smart-hub-laban",
+  projectId: "learn-smart-hub-laban",
   storageBucket: "learn-smart-hub-laban.firebasestorage.app",
-  messagingSenderId:  "707789494610",
+  messagingSenderId: "707789494610",
   appId: "1:707789494610:web:ee7f3f9a6fddbc913e028b"
 };
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
